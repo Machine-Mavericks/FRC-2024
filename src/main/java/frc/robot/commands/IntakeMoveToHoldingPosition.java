21e14e48e7af79de238e998c18cd5d43b998bc05
superzokabear <superzokabear@gmail.com>
2024-02-25 18:38:33 -0500

    Newist intake and shoot comands

--- src/main/java/frc/robot/commands/IntakeMoveToHoldingPosition.java
+++ src/main/java/frc/robot/commands/IntakeMoveToHoldingPosition.java
@@ -4,22 +4,18 @@
 
 package frc.robot.commands;
 
-import javax.lang.model.util.ElementScanner14;
-
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 
-public class SourceIntake extends Command {
-  
-  private Timer intakeTimer;
+public class IntakeMoveToHoldingPosition extends Command {
+ private Timer intakeTimer;
 
-  
-  /** Creates a new SourceIntake. */
-  public SourceIntake() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  /** Creates a new IntakeMoveToHoldingPosition. */
+  public IntakeMoveToHoldingPosition() {
     addRequirements(RobotContainer.cassetteintake); // add effector
     intakeTimer = new Timer();
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
@@ -27,28 +23,25 @@ public class SourceIntake extends Command {
   public void initialize() {
     intakeTimer.reset();
     intakeTimer.start();
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //set angle to source
-    RobotContainer.cassetteintake.intakeRun(1);
-    RobotContainer.cassetteintake.intakeRun(1);
+    RobotContainer.cassetteintake.intakeRun(-0.4);
+    RobotContainer.cassetteintake.intakeRun(-0.4);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     RobotContainer.cassetteintake.intakeRun(0);
-    //set angle to neutral
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(intakeTimer.hasElapsed(3)==true)
+    if(intakeTimer.hasElapsed(0.125)==true)
     return true;
     else 
     return false;
--- src/main/java/frc/robot/subsystems/CassetteIntake.java
+++ src/main/java/frc/robot/subsystems/CassetteIntake.java
@@ -19,6 +19,7 @@ public class CassetteIntake extends SubsystemBase {
 
   /* Intake speed in rps (Doesn't account for mechanism gearing on it's own) */
   private static double INTAKE_SPEED = 1; // TODO: tune
+  
                                                                                                                   
   /** Creates a new CassetteIntake. */
   public CassetteIntake() {
@@ -36,7 +37,7 @@ public class CassetteIntake extends SubsystemBase {
    * Start or stop intake at constant (tested) speed
    * @param state Use -1 to invert, 0 to stop, and 1 to run forward
   */
-  public void intakeRun(int state) {
+  public void intakeRun(double state) {
     m_IntakeMotor1.set(INTAKE_SPEED * state); // Todo, this uses percent output, not velocity control as it should
     m_IntakeMotor2.set(INTAKE_SPEED * state);
   }
