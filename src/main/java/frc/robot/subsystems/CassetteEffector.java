87d8da2af2005b11faa676ca00e62ba09bf2617b
Toast-Bucket <71938947+Kabinet0@users.noreply.github.com>
2024-03-02 20:51:28 -0500

    Drop prop on enable

--- src/main/java/frc/robot/subsystems/CassetteEffector.java
+++ src/main/java/frc/robot/subsystems/CassetteEffector.java
@@ -53,6 +53,7 @@ public class CassetteEffector extends SubsystemBase implements ShuffleUser {
   //TODO: figure out angle values
   public static final double MAX_TOP_ANGLE = 0.26;
   public static final double MIN_BOTTOM_ANGLE = 0.023;
+  public static final double DROP_PROP_ANGLE = 0.1;
   public static final double NEUTRAL_ANGLE = 0.05;
   public static final double GROUND_ANGLE = 0.022;
   public static final double SOURCE_ANGLE = 0.05;
@@ -136,7 +137,10 @@ public class CassetteEffector extends SubsystemBase implements ShuffleUser {
     SubsystemShuffleboardManager.RegisterShuffleUser(this);
 
     // Go to default angle
-    setAngle(NEUTRAL_ANGLE);
+    //setAngle(NEUTRAL_ANGLE);
+
+    // Go up to drop prop
+    setAngle(DROP_PROP_ANGLE);
   }
 
   /**
