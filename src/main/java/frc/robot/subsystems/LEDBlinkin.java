751ced6725ac18601e9d4a8a0a9ae65e26cf301e
KittenCat1080ti <71938947+KittenCat1080ti@users.noreply.github.com>
2024-03-08 14:41:15 -0500

    Fix blinkin according to manual (untested)

--- src/main/java/frc/robot/subsystems/LEDBlinkin.java
+++ src/main/java/frc/robot/subsystems/LEDBlinkin.java
@@ -6,19 +6,20 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 import frc.robot.RobotMap;
 
 public class LEDBlinkin extends SubsystemBase {
   //Victor led;
-  PWM led;
+  Spark led;
 
   /** Creates a new LEDBlinkin. */
   public LEDBlinkin() {
       // set up pwm channel
       //led = new Victor(RobotMap.PWMPorts.LED_BLINKIN);
-      led = new PWM(RobotMap.PWMPorts.LED_BLINKIN);
+      led = new Spark(RobotMap.PWMPorts.LED_BLINKIN);
   }
 
   @Override
@@ -59,29 +60,30 @@ public class LEDBlinkin extends SubsystemBase {
     switch (pattern) {
       
       case OFF:
-        led.setSpeed(0.99);    // black
-      break;
+        led.set(0.99);    // black
+        break;
       case LOWBATTERY:
-        led.setSpeed(0.91);    // solid purple
-      break;
+        led.set(0.91);    // solid purple
+        break;
       case DISCO:
-        led.setSpeed(-0.45);   // color wave - rainbow
+        led.set(-0.45);   // color wave - rainbow
+        break;
       case REDALLIANCE:
-        led.setSpeed(0.360); // red 
-      break;
+        led.set(0.360); // red 
+        break;
       case BLUEALLIANCE:
-        led.setSpeed(0.240); // blue 
-      break; 
+        led.set(0.240); // blue 
+        break; 
       case SEESNOTES:
-        led.setSpeed(-0.120); // pink
-      break;
+        led.set(-0.120); // pink
+        break;
       case APRILTAGS:
-        led.setSpeed(0.120); //green
-      break;
-     
-
+        led.set(0.120); //green
+        break;
+      default:
+        led.set(0.99);    // black
+        break;
     }
-    
   }
 
 
