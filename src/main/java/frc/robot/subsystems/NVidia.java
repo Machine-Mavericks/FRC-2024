951821bc9154f086976048842a5bc31847ef433d
lilyen06 <lilyemma06@gmail.com>
2024-03-16 16:11:04 -0400

    Add 3rd Nvidia camera

--- src/main/java/frc/robot/subsystems/NVidia.java
+++ src/main/java/frc/robot/subsystems/NVidia.java
@@ -18,8 +18,9 @@ public class NVidia extends SubsystemBase {
   private NetworkTable m_table;
   private DoubleArraySubscriber m_CameraSub3;
   private DoubleArraySubscriber m_CameraSub4;
-  private DoubleArraySubscriber m_RobotPoseSub3;
-  private DoubleArraySubscriber m_RobotPoseSub4;
+  private DoubleArraySubscriber m_RobotPoseSub0;
+  private DoubleArraySubscriber m_RobotPoseSub1;
+  private DoubleArraySubscriber m_RobotPoseSub2;
   private double[] data;
 
   /** Creates a new NVidia. */
@@ -27,21 +28,34 @@ public class NVidia extends SubsystemBase {
     m_table = NetworkTableInstance.getDefault().getTable("Nvidia");
     m_CameraSub3=m_table.getDoubleArrayTopic("camera1").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
     m_CameraSub4=m_table.getDoubleArrayTopic("camera2").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
-    m_RobotPoseSub3=m_table.getDoubleArrayTopic("robot_pose_in_field1").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
-    m_RobotPoseSub4=m_table.getDoubleArrayTopic("robot_pose_in_field2").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
+    m_RobotPoseSub0=m_table.getDoubleArrayTopic("robot_pose_in_field0").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
+    m_RobotPoseSub1=m_table.getDoubleArrayTopic("robot_pose_in_field1").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
+    m_RobotPoseSub2=m_table.getDoubleArrayTopic("robot_pose_in_field2").subscribe(null, PubSubOption.pollStorage(5), PubSubOption.periodic(0.02));
   }
 
   @Override
   public void periodic() {
     // array to hold apriltag detection data
     TimestampedDoubleArray AprilTagDetectionData[];
-    AprilTagDetectionData= m_RobotPoseSub3.readQueue();
+
+    // read data for camera 0
+    AprilTagDetectionData= m_RobotPoseSub0.readQueue();
     // for each AprilTag detection in the list
     for (int j=0;j<AprilTagDetectionData.length; j++) {
       data = AprilTagDetectionData[j].value;
       RobotContainer.odometry.addVision(new Pose2d(data[0],data[1],new Rotation2d(data[2])), AprilTagDetectionData[j].value[3]);
     }
-    AprilTagDetectionData= m_RobotPoseSub4.readQueue();
+
+    // read data for camera 1
+    AprilTagDetectionData= m_RobotPoseSub1.readQueue();
+    // for each AprilTag detection in the list
+    for (int j=0;j<AprilTagDetectionData.length; j++) {
+      data = AprilTagDetectionData[j].value;
+      RobotContainer.odometry.addVision(new Pose2d(data[0],data[1],new Rotation2d(data[2])), AprilTagDetectionData[j].value[3]);
+    }
+
+    // read data for camera 2
+    AprilTagDetectionData= m_RobotPoseSub2.readQueue();
     // for each AprilTag detection in the list
     for (int j=0;j<AprilTagDetectionData.length; j++){
       data = AprilTagDetectionData[j].value;
