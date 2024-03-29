2f22fee41444849e611b8ec4adc281c8c45ecbdd
lilyen06 <lilyemma06@gmail.com>
2024-03-29 13:52:17 -0400

    Success!

--- src/main/java/frc/robot/commands/SemiAutonomous/AutoHang.java
+++ src/main/java/frc/robot/commands/SemiAutonomous/AutoHang.java
@@ -5,7 +5,7 @@
 package frc.robot.commands.SemiAutonomous;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.Drive.AutoDriveToFieldPose;
+//import frc.robot.commands.Drive.AutoDriveToFieldPose;
 
 public class AutoHang extends Command {
   /** Creates a new AutoHang. */
